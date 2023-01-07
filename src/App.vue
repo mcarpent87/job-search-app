3a6af05814dc9f601eade348c6d958069d3bee43
mcarpent87 <mclean.carpenter@lendio.com>
2023-01-07 13:06:43 -0700

    initial commit for vue 3 course

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <h1>POOP</h1>
+  </main>
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
