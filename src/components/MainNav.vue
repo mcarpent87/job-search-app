6ffffff6ab45b28b159ec85920ebdfd8a6fd58c8
mcarpent87 <mclean.carpenter@lendio.com>
2023-01-09 10:49:43 -0700

    data method

---
+++ src/components/MainNav.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 w-full h-16 bg-white">
+      <div
+        class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 m-8"
+      >
+        <a href="" class="flex h-full items-center text-xl">
+          {{ company }}
+        </a>
+        <h2 class="ml-8 flex h-full items-center">
+          Developed By: {{ author.firstName }} {{ author.lastName }}
+        </h2>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+  data() {
+    return {
+      company: "Silicon Slopes Careers",
+      author: {
+        firstName: "McLean",
+        lastName: "Carpenter",
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
